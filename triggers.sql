0e589237c9d5b4a3bcc1263ad5bad5df7ee96f65
Deborange <101590106+DeboraBee@users.noreply.github.com>
2025-05-22 21:43:49 -0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,15 +1,11 @@
--- Questão 1--
-CREATE TRIGGER dbo.credits_earned
+-- Questão 1, trigger que atualiza o valor de créditos de um aluno após a retirada de um curso da sua lista. --
+CREATE TRIGGER dbo.lost_credits
 ON dbo.takes
-AFTER INSERT AS
+AFTER delete AS
 IF (ROWCOUNT_BIG() = 0)
 RETURN;
 BEGIN
 	UPDATE dbo.student
-    SET tot_cred = tot_cred + (SELECT credits FROM dbo.course INNER JOIN inserted ON course.course_id = inserted.course_id) 
-    WHERE student.id = (SELECT DISTINCT ID FROM inserted);	
+    SET tot_cred = tot_cred - (SELECT credits FROM dbo.course INNER JOIN deleted ON course.course_id = deleted.course_id) 
+    WHERE student.id = (SELECT DISTINCT ID FROM deleted);	
 END
-
-SELECT ID, count(*) AS qtd_courses FROM takes 
-GROUP BY ID
-ORDER BY qtd_courses;
