6ad9147f092806e5632cdafcd2a83386653f6edd
Deborange <101590106+DeboraBee@users.noreply.github.com>
2025-05-22 21:39:21 -0300

    Create triggers.sql

---
+++ triggers.sql
@@ -0,0 +1,14 @@
+CREATE TRIGGER dbo.credits_earned
+ON dbo.takes
+AFTER INSERT AS
+IF (ROWCOUNT_BIG() = 0)
+RETURN;
+BEGIN
+	UPDATE dbo.student
+    SET tot_cred = tot_cred + (SELECT credits FROM dbo.course INNER JOIN inserted ON course.course_id = inserted.course_id) 
+    WHERE student.id = (SELECT DISTINCT ID FROM inserted);	
+END
+
+SELECT ID, count(*) AS qtd_courses FROM takes 
+GROUP BY ID
+ORDER BY qtd_courses;
